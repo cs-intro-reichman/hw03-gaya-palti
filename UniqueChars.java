904ee0caa10a90c3cdd464658a200b7152148b9b
gaya-palti <gaya.palti@post.runi.ac.il>
2024-01-04 19:04:39 +0200

    UniqueChars

--- UniqueChars.java
+++ UniqueChars.java
@@ -4,14 +4,23 @@ public class UniqueChars {
         String str = args[0];
         System.out.println(uniqueChars(str));
     }
-
     /**
      * Returns a string which is identical to the original string, 
      * except that all the duplicate characters are removed,
      * unless they are space characters.
      */
-    public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
-    }
+    public static String uniqueChars(String str) {
+        String ans = "";
+        for (int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if (ch == ' '){
+                ans = ans + " ";
+            } else if (ans.indexOf(ch) == -1){
+                ans = ans + ch;
+            }
+        }
+        return ans;
+    }   
 }
+
+//check with indexof on the new string slide 32
