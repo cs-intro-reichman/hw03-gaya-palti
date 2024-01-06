897bfe65bdebfdec4fc574a4ac3e6ffd1c713bb6
gaya-palti <gaya.palti@post.runi.ac.il>
2024-01-06 17:26:43 +0200

    Calender 0

--- Calendar0.java
+++ Calendar0.java
@@ -22,13 +22,14 @@ public class Calendar0 {
 
 	// Tests the nDaysInMonth function.
 	private static void nDaysInMonthTest(int year) {
-		// Replace this comment with your code
+		for (int month = 1; month <= 12; month ++){
+			System.out.println("Month " + month + " has " + nDaysInMonth(month, year) + " days");
+		}
 	}
 
 	// Returns true if the given year is a leap year, false otherwise.
 	public static boolean isLeapYear(int year) {
-	    // Replace the following statement with your code
-		return false;
+		return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
 	}
 	 
 	// Returns the number of days in the given month and year.
@@ -36,8 +37,27 @@ public class Calendar0 {
 	// February has 28 days in a common year, and 29 days in a leap year.
 	// All the other months have 31 days.
 	public static int nDaysInMonth(int month, int year) {
-		// Replace the following statement with your code
-		return 0;
+		int days = 31;
+		switch (month) {
+			case 1: days = 31; break;
+			case 2: if (isLeapYear(year)) {
+						days = 29; 	
+					} else {
+						days = 28; 
+					} 
+					break;
+			case 3: days = 31; break;
+			case 4: days = 30; break;					
+			case 5: days = 31; break;
+			case 6: days = 30; break;	
+			case 7: days = 31; break;
+			case 8: days = 31; break;
+			case 9: days = 30; break;
+			case 10: days = 31; break;	
+			case 11: days = 30; break;
+			case 12: days = 31; break;				
+			}
+		return days;
 	}
 }
 
